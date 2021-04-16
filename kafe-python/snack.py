6e5c7da8c5a9017d746297e5267ff4f7fe3ae658
Nur Muhammad 'Ainul Yaqin <64957624+masnurrm@users.noreply.github.com>
2021-04-16 09:39:02 +0700

    Create snack.py

---
+++ kafe-python/snack.py
@@ -0,0 +1,12 @@
+from menu_item import MenuItem
+
+class Snack(MenuItem):
+    def __init__(self, name, price, calorie_count):
+        super().__init__(name, price)
+        self.calorie_count = calorie_count
+    
+    def info(self):
+        return self.name + ': Rp' + str(self.price) + ' (' + str(self.calorie_count) + 'kCal)'
+    
+    def calorie_info(self):
+        print('kCal: ' + str(self.calorie_count))
