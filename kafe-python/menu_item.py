3d3bcc12d52114e1dd16e7f2b1e24f1337f9a9a5
Nur Muhammad 'Ainul Yaqin <64957624+masnurrm@users.noreply.github.com>
2021-04-16 09:38:53 +0700

    Create menu_item.py

---
+++ kafe-python/menu_item.py
@@ -0,0 +1,20 @@
+class MenuItem:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def info(self):
+        return self.name + ': $' + str(self.price)
+
+    def get_total_price(self, count):
+        total_price = self.price * count
+
+        if count >= 3:
+            total_price *= 0.9
+
+        return round(total_price)
+
+    def get_total_snack_price(self, count):
+        total_price = self.price * count
+
+        return round(total_price)
